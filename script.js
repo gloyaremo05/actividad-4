b0b1c61278740ae0e584ad7c6abb05946366abc9
gloyaremo <gloyaremo@hotmail.com>
2021-09-26 21:10:44 -0500

    aqui subi la actividad 4

---
+++ script.js
@@ -0,0 +1,96 @@
+/*jaquery*/
+
+$(" .China ").mouseover(
+    function(){
+        $(".China").css("color","orange").css("font-size","40px")
+    }
+)
+
+
+
+
+$(".Singapur").click(
+    function(){
+        $(".Singapur").css("font-size","30px").css("color","blue")
+    }
+)
+
+$(".Macao").dblclick(
+    function(){
+        $(".Macao").css("background","yellow").css("color","green")
+    }
+)
+
+$(".HongKong").mouseover(
+    function(){
+        $(".HongKong").css("background","red")
+
+    }
+
+)
+
+$(".HongKong").mouseout(
+    function(){
+        $(".HongKong").css("background","pink")
+
+    }
+
+)    
+
+$(".Estonia").dblclick(
+    function(){
+        $(".Estonia").css("background","lightblue").css("font-size","50px")
+    }
+)
+
+$(".Canada").mouseover(
+    function(){
+        $(".Canada").css("background","purple").css("color","red")
+    }
+)
+
+$(".Finlandia").click(
+    function(){
+        $(".Finlandia").css("font-size","25px").css("color","pink")
+    }
+)
+
+
+$(".Irlanda").mouseover(
+    function(){
+        $(".Irlanda").css("font-size","60px").css("color","gray")
+    }
+)
+
+
+$(".Corea").dblclick(
+    function(){
+        $(".Corea").css("font-style","Comic Sans").css("font-size","30px").css("color","yellow")    
+    }
+)
+
+$(".Polonia").mouseover(
+    function(){
+        $(".Polonia").css("font-style","italic").css("color","darkblue")
+    }
+)
+$(".parrafo").mouseover(
+    function(){
+        $(".parrafo").css("background","green")
+          
+        
+    }
+)
+
+
+$(".parrafo2").mouseover(
+    function(){
+        $(".parrafo2").css("background","lightblue")
+    }
+)
+
+$(".parrafo3").mouseover(
+    function(){
+        $(".parrafo3").css("background","lightgreen") 
+    }
+)
